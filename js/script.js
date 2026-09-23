f79dc49b4dd0f0398029a2a51f879d545ce422dc
Albinus Soren <albinussor@gmail.com>
2026-09-23 21:00:25 +0530

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll("form").forEach(function (form) {
+        form.addEventListener("submit", function () {
+            const button = form.querySelector("button");
+            if (button) {
+                button.disabled = true;
+                button.innerText = "Uploading...";
+            }
+        });
+    });
+});
